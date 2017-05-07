ee930f4e1e4c64fe49c1cd9062b01efc3815d46c
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-05-07 11:02:34 +0600

    add logging

--- src/main/java/com/github/insanusmokrassar/AbstractDatabaseORM/Example.java
+++ src/main/java/com/github/insanusmokrassar/AbstractDatabaseORM/Example.java
@@ -1,7 +1,17 @@
 package com.github.insanusmokrassar.AbstractDatabaseORM;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.*;
+
 public class Example {
     public static void main(String[] args) {
-        System.out.println("Hello:)");
+        try(InputStream is = Example.class.getResourceAsStream(args[0])) {
+            LogManager.getLogManager().readConfiguration(is);
+        } catch (IOException | IndexOutOfBoundsException | NullPointerException e) {
+            System.err.println("Now logging is switched off because config was not found or path is not set:" + e.getMessage());
+            e.printStackTrace();
+        }
+        Logger.getGlobal().finest("HelloWorld");
     }
 }

6c2a68a8d9e3699fe0ba8d8668264eee36ed263a
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-05-23 11:47:15 +0600

    add config and config template, them load

--- src/main/kotlin/com/github/insanusmokrassar/AbstractDatabaseORM/Example.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AbstractDatabaseORM/Example.kt
@@ -1,6 +1,10 @@
 package com.github.insanusmokrassar.AbstractDatabaseORM
 
+import com.github.insanusmokrassar.AbstractDatabaseORM.core.ConfigReader
+import com.github.insanusmokrassar.IObjectKRealisations.JSONIObject
+import java.io.File
 import java.io.FileInputStream
+import java.util.*
 import java.util.logging.LogManager
 import java.util.logging.Logger
 
@@ -14,5 +18,13 @@ fun main(args: Array<String>) {
     } catch (e: Exception) {
         println("Can't load logger preferences: {$e}")
     }
+    val configStringBuffer = StringBuffer()
+    val scanner = Scanner(File(args[1]))
+    while (scanner.hasNextLine()) {
+        val current : String = scanner.nextLine()
+        configStringBuffer.append("$current\n\r")
+    }
+    val config = JSONIObject(configStringBuffer.toString())
+    ConfigReader(config)
     Logger.getGlobal().info("HelloWorld")
 }

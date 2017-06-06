0d3383d14e4a06b2b84cfd78c9c06e879e4f3d74
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-06-06 23:32:31 +0600

    remove DatabaseManager

--- src/main/kotlin/com/github/insanusmokrassar/AutoORM/core/DatabasesManager.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoORM/core/DatabasesManager.kt
@@ -4,68 +4,67 @@ import com.github.insanusmokrassar.AutoORM.core.drivers.databases.interfaces.Dat
 import com.github.insanusmokrassar.iobjectk.interfaces.IObject
 import kotlin.reflect.full.isSuperclassOf
 
-class DatabaseManager(config : IObject<Any>) {
-
-    private val databaseDrivers: MutableMap<String, DatabaseDriver> = HashMap()
-    val databasesPools: Map<String, ConnectionsPool>
-    private val driversConfigs: List<IObject<Any>> = config.get<List<Any>>("drivers").filter {
+fun createDatabasesPool(config : IObject<Any>): Map<String, ConnectionsPool> {
+    val driversConfigs: List<IObject<Any>> = config.get<List<Any>>("drivers").filter {
         it is IObject<*>
     } as List<IObject<Any>>
-    private val databasesConfigs: List<IObject<Any>> = config.get<List<Any>>("databases").filter {
+    val databasesConfigs: List<IObject<Any>> = config.get<List<Any>>("databases").filter {
         it is IObject<*>
     } as List<IObject<Any>>
-
-    init {
-        val pools = HashMap<String, ConnectionsPool>()
-        databasesConfigs.forEach {
-            val driver = getDatabaseDriver(it.get("driver"))
-            val currentConfig = it
-            pools.put(
-                    it.get<String>("name"),
-                    ConnectionsPool {
-                        onFree: (DatabaseConnect) -> Unit,
-                        onClose: (DatabaseConnect) -> Unit ->
-                        if (currentConfig.keys().contains("connections")) {
-                            val connections = ArrayList<DatabaseConnect>()
-                            for (i: Int in 0..currentConfig.get<Int>("connections") - 1) {
-                                connections.add(
-                                        driver.getDatabaseConnect(
-                                                currentConfig.get("config"),
-                                                onFree,
-                                                onClose
-                                        )
-                                )
-                            }
-                            connections
-                        } else {
-                            listOf(driver.getDatabaseConnect(
-                                    currentConfig.get("config"),
-                                    onFree,
-                                    onClose
-                            ))
+    val databaseDrivers: MutableMap<String, DatabaseDriver> = HashMap()
+    val databasesPools = HashMap<String, ConnectionsPool>()
+    databasesConfigs.forEach {
+        val driver = getDatabaseDriver(it.get("driver"), databaseDrivers, driversConfigs)
+        val currentConfig = it
+        databasesPools.put(
+                it.get<String>("name"),
+                ConnectionsPool {
+                    onFree: (DatabaseConnect) -> Unit,
+                    onClose: (DatabaseConnect) -> Unit ->
+                    if (currentConfig.keys().contains("connections")) {
+                        val connections = ArrayList<DatabaseConnect>()
+                        for (i: Int in 0..currentConfig.get<Int>("connections") - 1) {
+                            connections.add(
+                                    driver.getDatabaseConnect(
+                                            currentConfig.get("config"),
+                                            onFree,
+                                            onClose
+                                    )
+                            )
                         }
+                        connections
+                    } else {
+                        listOf(driver.getDatabaseConnect(
+                                currentConfig.get("config"),
+                                onFree,
+                                onClose
+                        ))
                     }
-            )
-        }
-        databasesPools = pools
+                }
+        )
     }
+    return databasesPools
+}
 
-    private fun getDatabaseDriver(name: String) : DatabaseDriver {
-        if (databaseDrivers.containsKey(name)) {
-            return databaseDrivers[name]!!
-        } else {
-            val config = driversConfigs.getFirst {
-                it.get<String>("name") == name
-            }?: throw IllegalArgumentException("Can't find config for database")
-            val parameters = config.get<Any>("config")
-            val driver = (Class.forName(config.get("classpath")).kotlin.constructors.getFirst {
-                it.parameters.size == 1 && (it.parameters[0].type.classifier as kotlin.reflect.KClass<*>).isSuperclassOf(parameters::class)
-            }?.call(
-                    parameters
-            )?: throw IllegalArgumentException("Can't find config for driver $name"))
-                    as? DatabaseDriver ?: throw IllegalStateException("Founded driver for name $name is not DatabaseDriver")
-            databaseDrivers.put(name, driver)
-            return driver
-        }
+private fun getDatabaseDriver(
+        name: String,
+        databaseDrivers:
+        MutableMap<String, DatabaseDriver>,
+        driversConfigs: List<IObject<Any>>) : DatabaseDriver {
+    if (databaseDrivers.containsKey(name)) {
+        return databaseDrivers[name]!!
+    } else {
+        val config = driversConfigs.getFirst {
+            it.get<String>("name") == name
+        }?: throw IllegalArgumentException("Can't find config for database")
+        val parameters = config.get<Any>("config")
+        val driver = (Class.forName(config.get("classpath")).kotlin.constructors.getFirst {
+            it.parameters.size == 1 && (it.parameters[0].type.classifier as kotlin.reflect.KClass<*>).isSuperclassOf(parameters::class)
+        }?.call(
+                parameters
+        )?: throw IllegalArgumentException("Can't find config for driver $name"))
+                as? DatabaseDriver ?: throw IllegalStateException("Founded driver for name $name is not DatabaseDriver")
+        databaseDrivers.put(name, driver)
+        return driver
     }
 }
